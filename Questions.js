2b2c4b245e645797d5615b64c8e7d800ef1d5cd9
Chhaya9094 <45236944+Chhaya9094@users.noreply.github.com>
2018-12-04 00:01:37 +0530

    Create Questions.js

---
+++ Questions.js
@@ -0,0 +1,7 @@
+var questions = [
+  ["Batman Favorite Enemy?", "The Joker", "Magneto", "The Green Goblin","None", "3"],
+  ["What is Cyclops’ power in X-Men?", "Telekinesis", "Optic blast", " Diamond skin", "all","2"],
+  ["Time Travel Gadget?", "the Tesseract", "the Magic Cube", "the Mjolnir", "all","1"],
+  [" How many Avengers are there in the Avengers INfinity war?", " 4", " 10", "6", "100","3"],
+  ["How do the Fantastic Four get their powers sky?", "  they were bitten by a radioactive spider", " they were exposed to cosmic radiation in space", " they were experimented on", "Dont know","2"],
+  ];
